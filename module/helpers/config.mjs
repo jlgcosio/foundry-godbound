f6708cb43f7acec7a87d190ab0a44b5d6cd39217
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-03-05 18:41:39 +0800

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const GODBOUND = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+GODBOUND.abilities = {
+  str: 'GODBOUND.Ability.Str.long',
+  dex: 'GODBOUND.Ability.Dex.long',
+  con: 'GODBOUND.Ability.Con.long',
+  int: 'GODBOUND.Ability.Int.long',
+  wis: 'GODBOUND.Ability.Wis.long',
+  cha: 'GODBOUND.Ability.Cha.long',
+};
+
+GODBOUND.abilityAbbreviations = {
+  str: 'GODBOUND.Ability.Str.abbr',
+  dex: 'GODBOUND.Ability.Dex.abbr',
+  con: 'GODBOUND.Ability.Con.abbr',
+  int: 'GODBOUND.Ability.Int.abbr',
+  wis: 'GODBOUND.Ability.Wis.abbr',
+  cha: 'GODBOUND.Ability.Cha.abbr',
+};
